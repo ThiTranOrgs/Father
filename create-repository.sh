473f3da1f4b4cbb15d95a21a6b6e8d31e076bf87
QuocThi <quocthi0606@gmail.com>
2023-09-20 18:14:30 +0700

    fix 7

--- create-repository.sh
+++ create-repository.sh
@@ -24,14 +24,23 @@ if [ $RETURN_CODE -ne 0 ]; then
 	return 1
 fi
 
-# Send a invitation to contract owner account
+# Send a invitation to contract owner account to be an organiztion members
+# curl -L \
+# 	-X POST \
+# 	-H "Accept: application/vnd.github+json" \
+# 	-H "Authorization: Bearer $TOKEN" \
+# 	-H "X-GitHub-Api-Version: 2022-11-28" \
+# 	https://api.github.com/orgs/$ORG_OWNER/invitations \
+# 	-d "{\"invitee_id\":$CONTRACT_OWNER,\"role\":\"direct_member\"}"
+
+# Send a invitation to contract owner account to be a repository collaborator
 curl -L \
-	-X POST \
+	-X PUT \
 	-H "Accept: application/vnd.github+json" \
 	-H "Authorization: Bearer $TOKEN" \
 	-H "X-GitHub-Api-Version: 2022-11-28" \
-	https://api.github.com/orgs/$ORG_OWNER/invitations \
-	-d "{\"invitee_id\":$CONTRACT_OWNER,\"role\":\"direct_member\"}"
+	https://api.github.com/repos/$ORG_OWNER/$REPO_NAME/collaborators/$CONTRACT_OWNER \
+	-d '{"permission":"push"}'
 
 RETURN_CODE=$?
 if [ $RETURN_CODE -ne 0 ]; then
