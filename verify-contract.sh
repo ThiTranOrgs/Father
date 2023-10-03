a88d9117f358af732e2c2712f1258290fe8557a8
QuocThi <quocthi0606@gmail.com>
2023-10-03 17:14:35 +0700

    update

--- verify-contract.sh
+++ verify-contract.sh
@@ -17,6 +17,7 @@ ARTIFACTS="$SOURCE_DIR/artifacts"
 NODE_RPC="--node tcp://tencent.blockchain.testnet.sharetoken.io:26657/ --chain-id ShareRing-LifeStyle"
 SHARELEDGER_BIN="shareledger"
 ENCRYPTED_CHECKSUM_FILE="/tmp/checksum.dat"
+PRIVKEY_FILE="/tmp/key.pem"
 # TODO: Update path to secrets
 CODE_ID=$(<$CODE_ID_FILE)
 
@@ -86,9 +87,11 @@ CODE_ID=$(<$CODE_ID_FILE)
 # if [ "$LOCAL_CHECKSUM" = "$BLOCK_CHAIN_CHECKSUM" ]; then
 # Encrypt checksumm
 LOCAL_CHECKSUM="70549b63a5e28d741bf3f1d1afe0d7e971036b7a8d9849b4298d70718b4be0f3"
-KEY=$(echo $PRIVATE_KEY | base64 -d)
+echo -n "$PRIVATE_KEY" | base64 -d >$PRIVKEY_FILE
+cat $PRIVKEY_FILE
+KEY=$(echo -n $PRIVATE_KEY | base64 -d)
 echo ".$KEY."
-echo "$LOCAL_CHECKSUM" | openssl dgst -sha256 -sign <(echo -n "$KEY") -out $ENCRYPTED_CHECKSUM_FILE
+echo "$LOCAL_CHECKSUM" | openssl dgst -sha256 -sign $PRIVKEY_FILE -out $ENCRYPTED_CHECKSUM_FILE
 BASE_64=$(openssl base64 -in $ENCRYPTED_CHECKSUM_FILE)
 echo "=======$BASE_64"
 # Remove encrypted checksum output file
