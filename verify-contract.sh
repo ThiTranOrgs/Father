4582871c36c61dcb7d5272b871a539d8e3f115da
QuocThi <quocthi0606@gmail.com>
2023-10-03 16:56:57 +0700

    update

--- verify-contract.sh
+++ verify-contract.sh
@@ -86,7 +86,8 @@ CODE_ID=$(<$CODE_ID_FILE)
 # if [ "$LOCAL_CHECKSUM" = "$BLOCK_CHAIN_CHECKSUM" ]; then
 # Encrypt checksumm
 LOCAL_CHECKSUM="70549b63a5e28d741bf3f1d1afe0d7e971036b7a8d9849b4298d70718b4be0f3"
-echo "$LOCAL_CHECKSUM" | openssl dgst -sha256 -sign <(echo "$PRIVATE_KEY") -out $ENCRYPTED_CHECKSUM_FILE
+KEY=$(echo $PRIVATE_KEY | base64 -d)
+echo "$LOCAL_CHECKSUM" | openssl dgst -sha256 -sign <(echo -n "$KEY") -out $ENCRYPTED_CHECKSUM_FILE
 BASE_64=$(openssl base64 -in $ENCRYPTED_CHECKSUM_FILE)
 echo "=======$BASE_64"
 # Remove encrypted checksum output file
